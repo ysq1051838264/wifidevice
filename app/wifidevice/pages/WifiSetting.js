955e377efb5083f4fb96ef765b89c94dd50dca38
ysq <1051838264@qq.com>
2017-06-16 14:00:27 +0800

    修复bug

--- app/wifidevice/pages/WifiSetting.js
+++ app/wifidevice/pages/WifiSetting.js
@@ -28,7 +28,6 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import QNButton from '../component/QNButton';
 import NavigationBar from '../component/NavigationBar';
 import WifiConfigSecond from './WifiConfigSecond';
-import EnterWeightView from './EnterWeightView';
 
 var {SmartLinker, QNDeviceInfo} = NativeModules;
 
@@ -149,21 +148,10 @@ export default class WifiSetting extends Component {
                 </View>
 
                 <View style={styles.bottomBar}>
-                    <QNButton color={this.state.themeColor} title={"开始配网"} onPress={this.toEnterWeight.bind(this)}/>
+                    <QNButton color={this.state.themeColor} title={"开始配网"} onPress={this.startConfig.bind(this)}/>
                 </View>
             </View>
         );
-    }
-
-    toEnterWeight() {
-        const {navigator} = this.props;
-        navigator.push({
-            component: EnterWeightView,
-            name: 'enter_weight',
-            params: {
-                themeColor: this.state.themeColor,
-            }
-        });
     }
 }
 
